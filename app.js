0100bbb6e396c91fb4aeb8848e6e48e46d75dd7c
Cody Leedom <33756741+Uruktek@users.noreply.github.com>
2022-03-05 07:38:33 -0600

    Added top bar and menu for timer choices

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('beep');
+var collapsibleMenuContent = document.getElementsByClassName("collapsible");
+
+for (let index = 0; index < collapsibleMenuContent.length; index++) {
+    collapsibleMenuContent[index].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        console.log(content)
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
