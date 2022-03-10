5c752c85af12c13480868d1f0321e8f22be0a5ec
Cody Leedom <33756741+Uruktek@users.noreply.github.com>
2022-03-09 20:19:42 -0600

    update stuff

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 let collapsibleMenuContent = document.getElementsByClassName("collapsible");
 for (let index = 0; index < collapsibleMenuContent.length; index++) {
-    collapsibleMenuContent[index].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
+  collapsibleMenuContent[index].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
+let currentDate = new Date().getTime();
+currentDate + 60000; //testing
 let buttonsOfTimer = document.getElementsByClassName("1min");
-buttonsOfTimer[0].addEventListener("click",function(){
-    setInterval(function(){
-        let currentTime = new Date();
-        document.getElementById("clock").style.color = 'red';
-        document.getElementById("clock").innerHTML = currentTime.toLocaleDateString();
-    },1000);
-});
+buttonsOfTimer[0].addEventListener(
+  "click",
+  function () {
+    let now = new Date().getTime();
+    let distance = now - currentDate;
+    console.log(distance);
+    if (distance < 0) {
+      console.log("it works?");
+    }
+  },
+  1000
+);
